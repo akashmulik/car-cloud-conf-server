ff3f7a16d4cca0e3735499a654d8c9ab9e6e7d53
Akash Mulik <24788556+akashmulik@users.noreply.github.com>
2020-09-10 15:42:13 +0530

    uploading project

---
+++ src/main/java/car/cloud/config/CarCloudConfigApplication.java
@@ -0,0 +1,15 @@
+package car.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CarCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCloudConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/car/cloud/config/CarCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package car.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
